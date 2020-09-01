0f695bef0c42f2b174083a83c8e709b60dc31785
John Orozco <john.o111@yahoo.com>
2020-09-01 17:02:00 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot (name = "Squawk!")
- if name = " "
- puts "Squawk!"
- return "Squawk!"
- else
- puts "Pretty bird!"
- return "Pretty bird!"
- end
+ puts name
+ return name
 end
